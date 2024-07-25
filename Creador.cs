9f0e8c6af78a2a85a162d655f961da9e5e65d45e
Giovanni <134148500+GiovanniPeirone@users.noreply.github.com>
2024-07-25 13:46:50 -0300

    h

---
+++ Creador.cs
@@ -0,0 +1,15 @@
+namespace CalcularCircuito
+{
+    public class Creador
+    {
+        int X,Y;
+
+        public Creador(int _x,int _Y)
+        {
+            X = _x;
+            Y = _Y;
+        }
+
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using CalcularCircuito;
+
+Console.CursorVisible = false;
+
+void CrearBateria()
+{
+    string bateria = """
+        █
+    ▄▄▄▄█▄▄▄▄
+      ▄▄▄▄▄
+        █
+    """;
+    Console.SetCursorPosition(5,30);
+    Console.WriteLine(bateria); 
+}
+
+while (true)
+{
+    Console.Clear();
+    CrearBateria();
+    Thread.Sleep(10);
+}
